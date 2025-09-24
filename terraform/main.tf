cd695b8dbbe6dabc2f1782b8c20ca3234747a296
Vladislav Tropnikov <vladislav.tropnikov@genestack.com>
2025-09-24 11:35:00 +0200

    add tf

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+locals {
+  helm_releases = {
+    velero = {
+      version    = "10.1.3"
+      chart      = "velero"
+      repository = "https://vmware-tanzu.github.io/helm-charts"
+      values = [
+        <<-EOT
+        initContainers:
+          - name: velero-plugin-for-aws
+            # renovate: datasource=github-releases depName=velero/velero-plugin-for-aws
+            image: velero/velero-plugin-for-aws:v1.12.1
+            imagePullPolicy: IfNotPresent
+            volumeMounts:
+              - mountPath: /target
+                name: plugins
+        EOT
+      ]
+    }
+  }
+}
