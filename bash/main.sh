1896bdd1a4b2a409c354d85017eaf0104968324f
Vladislav Tropnikov <vladislav.tropnikov@genestack.com>
2025-09-24 12:22:24 +0200

    test

--- bash/main.sh
+++ bash/main.sh
@@ -3,6 +3,5 @@
 # renovate: datasource=github-releases depName=helm/helm
 HELM_VERSION="v3.17.3"
 
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-chmod 700 get_helm.sh
-./get_helm.sh --no-sudo --version "$HELM_VERSION"
+# renovate: datasource=github-releases depName=terraform/terraform versioning=hashicorp
+TERRAFORM_VERSION="1.12.2"
